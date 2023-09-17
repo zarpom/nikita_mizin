11cde3dd92ec9e4cacae6f95b82995d2b197ca94
Vitalii Lytvynov <vyacheslavxxxxx@gmail.com>
2023-09-17 13:16:01 +0300

    lesson14: main for MethodExamples

--- src/main/java/lesson14/home/work/MethodExamples.java
+++ src/main/java/lesson14/home/work/MethodExamples.java
@@ -4,6 +4,31 @@ public class MethodExamples {
     // Введення до методів в Java
     // Методи в Java - це блоки коду, які виконують певну операцію та можуть бути викликані з інших частин програми.
 
+    public static void main(String[] args) {
+        int sum = addTwoNumbers(5, 7);
+        System.out.println("Сума двох чисел: " + sum);
+
+        int square = squareNumber(4);
+        System.out.println("Квадрат числа: " + square);
+
+        boolean isEvenNumber = isEven(6);
+        System.out.println("Число парне: " + isEvenNumber);
+
+        String concatenatedStr = concatenateStrings("Привіт, ", "Світ!");
+        System.out.println("Результат конкатенації рядків: " + concatenatedStr);
+
+        double average = averageOfThreeNumbers(10, 20, 30);
+        System.out.println("Середнє арифметичне трьох чисел: " + average);
+
+        int max = maxOfTwoNumbers(25, 15);
+        System.out.println("Максимальне число: " + max);
+
+        String grade = gradeFromScore(75);
+        System.out.println("Оцінка за балом: " + grade);
+
+        printBuddhaQuote();
+    }
+
     // Метод для додавання двох чисел
     public static int addTwoNumbers(int num1, int num2) {
         return num1 + num2;
@@ -36,19 +61,21 @@ public class MethodExamples {
 
     // Метод з кількома операторами повернення, що визначає оцінку на основі заданого балу
     public static String gradeFromScore(int score) {
-        int scoreResult = 0;
-        if(score >= 90) {
-            return "A";
-        } else if(score >= 80) {
-            return "B";
-        } else if(score >= 70) {
-            return "C";
-        } else if(score >= 60) {
-            return "D";
+        String grade;
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80) {
+            grade = "B";
+        } else if (score >= 70) {
+            grade = "C";
+        } else if (score >= 60) {
+            grade = "D";
         } else {
-            return "F";
+            grade = "F";
         }
+        return grade;
     }
+
 
     // Метод, що виводить буддійську цитату
     public static void printBuddhaQuote() {
